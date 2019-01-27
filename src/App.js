4f0499af0b0f8a37e2ef8b6b024f54c2f4e7d5bc
huangwuchao <471287451@qq.com>
2019-01-27 21:33:54 +0800

    首页布局

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './sass/common.scss'
+ import './sass/base.scss'
 import './sass/App.scss';
+//import Input from 'antd-mobile/lib/input-item/Input';
 
 class App extends Component {
   render() {
@@ -8,12 +9,16 @@ class App extends Component {
       <div className="App">
           <div className="top">
               <div className="top1">
-                <div className="left fenlei fa navicon">
-                 
+                <div className="left">
+                  {/* <img src={'./img/fenlei.png'} alt="" />   */}
+                  <a href="JavaScript:;" className="fa fa-angle-left">分类</a>
                 </div>
                 <span>
-                <img src={''} alt="" />
+                    <img src={''} alt="" />
                 </span>
+                <div classNam="right">
+                    <input></input>
+                </div>
               </div>
               <div className="top2"></div>
           </div>
